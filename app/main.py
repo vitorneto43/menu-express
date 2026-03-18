673619a4412e7cb971554f9ba34da88f864e4c3f
Vitor Veiga Vasconcelos Neto <108374481+vitorneto43@users.noreply.github.com>
2026-03-18 01:00:24 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -30,6 +30,7 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:5173",
         "http://localhost:5173",
+        "https://menu-express-nu.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
